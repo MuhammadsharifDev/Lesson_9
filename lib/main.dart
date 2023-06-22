62b65f305ee2c6785086d650c35333ab992fef6d
MuhammadsharifDev <muhammadsharifbozorov1005@gmail.com>
2023-06-22 20:25:14 +0500

    Some changed Namely This is about me (Shortly)!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:async';
+
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -24,14 +26,28 @@ class _MyAppState extends State<MyApp> {
 
           backgroundColor: Colors.blue,
         ),
-        body:SvgPicture.asset('assets/alert.svg',
-        width:500,
-        height:500),
+        body:Column(
 
 
-
+          children: [
+            Image.asset('assets/myself.jpg',
+            width:440,
+            height:420,
+            ),
+            Text('Name:Muhammadsharif',style:TextStyle(fontSize: 30)),
+            Text('Surname:Bozorov',style: GoogleFonts.acme(fontSize: 32),),
+            Text('Study:Tuit',style:GoogleFonts.abhayaLibre(fontSize:30)),
+            Text('Age:18',style:TextStyle(fontSize:32)),
+            Text('JOB:Student',style:TextStyle(fontSize: 30)),
+          ],
+        ),
+        // SvgPicture.asset('assets/alert.svg',
+        // width:500,
+        // height:500),
 
       ),
+
+
     );
   }
 }
